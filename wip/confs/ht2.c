451aa7bf16162f6ef2c4fad3024e08acde49d3d8
BreadyX <alex.bradatan85@yahoo.it>
2019-11-07 22:32:57 +0100

    cleaned macros in ht2.c

--- wip/confs/ht2.c
+++ wip/confs/ht2.c
@@ -5,18 +5,21 @@
 #include <errno.h>
 #include <time.h>
 
-#define TABLE_SIZE 1000
-#define VALUE_SIZE 10
-#define KEY_SIZE   10
-
-#define ENOTFOUND  -1
-
-#if (defined(HT_DEBUG) && HT_DEBUG != 0)
-    #define perr(...) fprintf(stderr, __VA_ARGS__)
-#else
-    #define perr(...) ;
+// CONSTATS
+#ifndef TABLE_SIZE
+    #define TABLE_SIZE 1000
+#endif
+#ifndef VALUE_SIZE
+    #define VALUE_SIZE 10
+#endif
+#ifndef KEY_SIZE
+    #define KEY_SIZE   10
 #endif
 
+// STATICS
+static int collisions;
+
+// TYPES
 struct Node {
     char value[VALUE_SIZE + 1];
     char key[KEY_SIZE + 1];
@@ -29,10 +32,21 @@ struct HashTable {
 typedef struct Node      Node_t;
 typedef struct HashTable HashTable_t;
 
+// FUNCTIONS
+#if (defined(HT_DEBUG) && HT_DEBUG != 0)
+    #define perr(...) fprintf(stderr, __VA_ARGS__)
+    #define pdebug(...) fprintf(stdout, __VA_ARGS__)
+    #define log_coll() do { \
+        fprintf(stdout, "Debug: hash collision\n"); collisions++;} while (0)
+#else
+    #define perr(...) ;
+    #define pdebug(...) ;
+    #define log_coll() collisions++
+#endif
 unsigned long HT_hash(const unsigned char key[KEY_SIZE]);
 HashTable_t *HT_create();
 void HT_destroy(HashTable_t *table);
-int HT_ins(HashTable_t *table, const char key[KEY_SIZE], 
+int HT_ins(HashTable_t *table, const char key[KEY_SIZE],
            const char value[VALUE_SIZE]);
 // HT_del
 int HT_has(HashTable_t *table, const char key[KEY_SIZE]);
@@ -51,11 +65,11 @@ unsigned long HT_hash(const unsigned char str[])
 
 HashTable_t* HT_create()
 {
-    /* Create a new HashTable of size TABLE_SIZE and return a pointer to it.
-     * 
+    /* Create a new HashTable and return a pointer to it.
+     *
      * Each item of the HashTable is a key-value pair that contains
-     * KEY_SIZE and VALUE_SIZE chars for key and value respectively 
-     * EXCLUDING the null terminator (key[KEY_SIZE + 1], 
+     * KEY_SIZE and VALUE_SIZE chars for key and value respectively
+     * EXCLUDING the null terminator (key[KEY_SIZE + 1],
      * value[VALUE_SIZE + 1]).
      */
     return malloc((unsigned long) sizeof(HashTable_t));
@@ -90,14 +104,14 @@ int HT_ins(HashTable_t *table, const char key[], const char value[])
      * Note 2: if `key` and `value` exceed KEY_SIZE + 1 and VALUE_SIZE + 1
      *         respectively, truncation will take place. If different sizes are
      *         needed, override the aforemetioned macros before usage.
-     */ 
+     */
     unsigned long h;
     Node_t *to_append;
-    
+
     if (!table) {
         perr("Null table passed to function\n");
         return EINVAL;
-    }    
+    }
     // Create new node
     to_append = malloc(sizeof(Node_t));
     if (!to_append) {
@@ -119,6 +133,7 @@ int HT_ins(HashTable_t *table, const char key[], const char value[])
             table->tails[h]->next = to_append;
             table->tails[h] = to_append;
         }
+        return 0;
     } else {
         perr("Item with key %s already present\n", to_append->key);
         free(to_append);
@@ -128,7 +143,7 @@ int HT_ins(HashTable_t *table, const char key[], const char value[])
 
 int HT_del(HashTable_t *table, const char key[])
 {
-    
+    return 0;
 }
 
 int HT_has(HashTable_t *table, const char key[])
@@ -137,7 +152,6 @@ int HT_has(HashTable_t *table, const char key[])
      * else return -1.
      * Note: if `key` is bigger than KEY_SIZE + 1, then it will be truncated.
      */
-    
     char san_key[KEY_SIZE + 1];
     unsigned long h;
     Node_t *current;
@@ -145,7 +159,7 @@ int HT_has(HashTable_t *table, const char key[])
     strncpy(san_key, key, (KEY_SIZE + 1) * sizeof(char));
     san_key[KEY_SIZE] = '\0';
     // Search
-    h = hash((unsigned char*) san_key);
+    h = HT_hash((unsigned char*) san_key);
     current = table->table[h];
     while (current) {
         if (strcmp(current->key, san_key) == 0)
@@ -155,4 +169,4 @@ int HT_has(HashTable_t *table, const char key[])
     return -1;
 }
 
-int main(void) { create_table(); return 0; }
+int main(void) { HT_create(); return 0; }
