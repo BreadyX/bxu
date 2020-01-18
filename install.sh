4ede5e2d8b677cd1d856a688d789c7d2f8a8813b
BreadyX <alex.bradatan85@yahoo.it>
2020-01-18 19:14:03 +0100

    Improved structure Improved structure of the project: Every project is under the src subdir.
    Now the 'install.sh' script doesn't need a hardwired list of
    directories, has better argument parsing and handles errors better.

--- install.sh
+++ install.sh
@@ -1,49 +1,55 @@
 #!/bin/sh -e
 
-INSTALLDIR="$HOME/.local/bin"
-PROJS="clean confs git-hud ic laptop-stuff"
-
-# Parse arguments
-case "$1" in
-	--otherbin)
-		printf "Setting install directory to %s\n" "$2"
-		INSTALLDIR="$2" ;;
-    "")
-        ;;
-	--help|*)
-		cat << EOF
-install.sh [--otherbin path]
+DOC="install.sh [--otherbin path]
 This scripts installs the BreadyX's Utils.
 
-By deafult it installs in "$INSTALLDIR", but it can be changed by passing the
-option --otherbin and giving a path.
-EOF
-		exit 0 ;;
-esac
+By default it installs in '$INSTALLDIR', but it can be changed by passing the
+option --otherbin and giving a path."
+INSTALLDIR="$HOME/.local/bin"
+PROJS="$(for dir in $(find src/ -type d | grep -v 'src/$'); do basename "$dir"; done)"
+
+ERRORS=0
+
+# Parse arguments
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --otherbin)
+            printf "Setting install directory to %s\n" "$2"
+            INSTALLDIR="$(realpath "$2")"
+            shift 2 ;;
+        "")
+            ;;
+        --help|*)
+            printf "%s\n" "$DOC"
+            exit 0 ;;
+    esac
+done
 
 # Prompt user
 printf "This script will install the BreadyX utils (BXU) inside %s\nProceed? [y/n] " "$INSTALLDIR"
 read -r i
-[ "$(echo "$i" | tr "[:upper]" "[:lower]")" = "n" ] && {
+if [ "$(echo "$i" | tr "[:upper]" "[:lower]")" = "n" ]; then
 	printf "Aborted by user\n"
 	exit 0
-}
+fi
 
 # Check for directory
-[ -d "$INSTALLDIR" ] || {
-	printf "Directory %s doesn't exists. Please create it\n" "$INSTALLDIR"
+if ! { [ -d "$INSTALLDIR" ] || [ -w "$INSTALLDIR" ]; } ; then
+	printf "Cannot access %s: directory doesn't exists or user hasn't access to it\n"\
+        "$INSTALLDIR"
 	exit 1
-}
+fi
 
 # Loop projects and run `setup.sh` in a subshell
 for proj in $PROJS; do
-    printf "Installing '%s':\n" "$proj"
-    if [ -d "$proj" ]; then
-        if [ -f "$proj/setup.sh" ]; then
-            (
-                cd "$proj"
-                sh setup.sh "$INSTALLDIR" | fold | sed 's/^/\t/'
-            )
+    proj_dir="./src/$proj"
+    printf "Installing '%s':\n" "$proj_dir"
+    if [ -d "$proj_dir" ]; then
+        if [ -f "$proj_dir/setup.sh" ] && [ -x "$proj_dir/setup.sh" ]; then
+            ( cd "$proj_dir"
+              sh setup.sh "$INSTALLDIR" | fold | sed 's/^/\t/'
+            ) || { printf "\tError in setup of %s\n" "$proj"; $((ERRORS++)); }
+              && printf "\tDone\n"
         else
             printf "\tMissing setup for project '%s'. Please file an issue\n" "$proj"
         fi
@@ -52,12 +58,3 @@ for proj in $PROJS; do
     fi
     printf "\n"
 done
-
-# get files
-# files="$(find ./utils/ -type f | tr "\n" " ")"
-
-# Make file executables if they are not
-# chmod +x $files
-
-# Copy files to INSTALLDIR
-# cp -ui $files "$INSTALLDIR"
