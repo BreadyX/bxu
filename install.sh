6ae78ac67aa1861235b5468ed68417cae3bcfc66
BreadyX <alex.bradatan85@yahoo.it>
2020-02-24 16:21:19 +0100

    Modified install script Install script has been improved: - Better option management:     * started using gnu getopt     * shortopt support     * added options: -l/--list, -x/--exclude     * changed --otherbin option to simply --bin (or -b) - Fixed error in project discovery - Improved error handling in the `setup.sh` subshell - Added colored output

--- install.sh
+++ install.sh
@@ -1,59 +1,79 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
-DOC="install.sh [--otherbin path]
-This scripts installs the BreadyX's Utils.
-
-By default it installs in '$INSTALLDIR', but it can be changed by passing the
-option --otherbin and giving a path."
 INSTALLDIR="$HOME/.local/bin"
-PROJS="$(for dir in $(find src/ -type d | grep -v 'src/$'); do basename "$dir"; done)"
+PROJS="$(for dir in $(find src/ -maxdepth 1 -type d | grep -v 'src/$'); do basename "$dir"; done)"
+EXCLUDE=""
+DOC="install.sh [ OPTIONS ]
+Installs the BreadyX's Utils.
 
-ERRORS=0
+    -b, --bin=PATH      Use this install directory instead of default
+	-x, --exclude=PROJ  Do not install these projects (comma separated)
+	-l, --list			List all installable projects and exit
+    -h, --help          Display this message and exit"
 
-# Parse arguments
-while [ $# -gt 0 ]; do
-    case "$1" in
-        --otherbin)
-            printf "Setting install directory to %s\n" "$2"
-            INSTALLDIR="$(realpath "$2")"
-            shift 2 ;;
-        "")
-            ;;
-        --help|*)
-            printf "%s\n" "$DOC"
-            exit 0 ;;
-    esac
+RED="$(tput setaf 1)"
+GREEN="$(tput setaf 2)"
+YELLOW="$(tput setaf 3)"
+GRAY="$(tput setaf 250)"
+BOLD="$(tput bold)"
+RESET="$(tput sgr0)"
+
+ARGS=$(getopt -n "install.sh" -o "hlb:x:" -l "help,list,bin:,exclude:" -- "$@")
+set -- $ARGS
+while true; do
+	case $1 in
+		-h|--help)
+			printf "%s\n" "$DOC"
+			exit 0 ;;
+		-b|--bin)
+			shift
+			INSTALLDIR="$(echo $1 | xargs)"
+			shift ;;
+		-l|--list)
+			shift
+			printf "%s\n" "$PROJS"
+			exit 0 ;;
+		-x|--exclude)
+			shift
+			read -a EXCLUDE -r < <(echo "$1" | sed "s/,/ /g" | xargs)
+			shift ;;
+		--)
+			shift
+			break ;;
+	esac
 done
 
-# Prompt user
 printf "This script will install the BreadyX utils (BXU) inside %s\nProceed? [y/n] " "$INSTALLDIR"
 read -r i
 if [ "$(echo "$i" | tr "[:upper]" "[:lower]")" = "n" ]; then
 	printf "Aborted by user\n"
 	exit 0
 fi
+printf "\n"
 
-# Check for directory
-if ! { [ -d "$INSTALLDIR" ] || [ -w "$INSTALLDIR" ]; } ; then
+if { ! [ -d "$INSTALLDIR" ]; } && { ! [ -w "$INSTALLDIR" ]; } ; then
 	printf "Cannot access %s: directory doesn't exists or user hasn't access to it\n"\
-        "$INSTALLDIR"
+		"$INSTALLDIR"
 	exit 1
 fi
 
-# Loop projects and run `setup.sh` in a subshell
 for proj in $PROJS; do
-    proj_dir="./src/$proj"
-    printf "Installing '%s':\n" "$proj_dir"
-    if [ -d "$proj_dir" ]; then
-        if [ -f "$proj_dir/setup.sh" ] && [ -x "$proj_dir/setup.sh" ]; then
-            ( cd "$proj_dir"
-              sh setup.sh "$INSTALLDIR" | fold | sed 's/^/\t/'
-            ) || { printf "\tError in setup of %s\n" "$proj"; $((ERRORS++)); } && printf "\tDone\n"
-        else
-            printf "\tMissing setup for project '%s'. Please file an issue\n" "$proj"
-        fi
-    else
-        printf "\tMissing project '%s'. Please file an issue\n" "$proj"
-    fi
-    printf "\n"
+	proj_dir="./src/$proj"
+	if [ "$EXCLUDE" != "${EXCLUDE#$proj}" ]; then # proj is in EXCLUDE
+		continue
+	fi
+	printf "%sInstalling '%s':%s\n" "$BOLD" "$proj" "$RESET"
+	if [ -x "$proj_dir/setup.sh" ]; then
+		( set +e
+		  cd "$proj_dir"
+		  out="$(sh -e setup.sh "$INSTALLDIR")" exit_code=$?
+		  printf "%s%s%s\n" "$GRAY" "$out" "$RESET" | fold | sed 's/^/\t/'
+		  if [ "$exit_code" != 0 ]; then
+			   printf "\t%sError in setup of %s%s\n" "$RED" "$proj" "$RESET"
+			   exit 1
+		  fi ) && printf "\t%sDone%s" "$GREEN" "$RESET"
+	else
+		printf "\t%sMissing setup for project '%s'%s\n" "$YELLOW" "$proj" "$RESET"
+	fi
+	printf "\n"
 done
