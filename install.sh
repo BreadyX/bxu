22b173ec355ce552817872750c3218c77a988200
BreadyX <alex.bradatan85@yahoo.it>
2020-02-24 17:58:13 +0100

    Install supports only meson The old setup.sh install method has been deprecated in favour of meson.
    To account or the change, the -b/--bin has been replaced with the
    -p/--prefix option to better reflect its intent

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/bin/bash -e
 
-INSTALLDIR="$HOME/.local/bin"
-PROJS="$(for dir in $(find src/ -maxdepth 1 -type d | grep -v 'src/$'); do basename "$dir"; done)"
+PREFIX="$HOME/.local/"
+BUILDDIR="./meson-build"
+PROJS="$(
+	for dir in $(find ./src/ -maxdepth 1 -type d | grep -v 'src/$'); do
+		basename "$dir"
+	done
+)"
 EXCLUDE=""
+
 DOC="install.sh [ OPTIONS ]
 Installs the BreadyX's Utils.
 
-    -b, --bin=PATH      Use this install directory instead of default
+Options:
+    -p, --prefix=PATH      Use this install directory instead of default
 	-x, --exclude=PROJ  Do not install these projects (comma separated)
 	-l, --list			List all installable projects and exit
     -h, --help          Display this message and exit"
@@ -18,16 +25,16 @@ GRAY="$(tput setaf 250)"
 BOLD="$(tput bold)"
 RESET="$(tput sgr0)"
 
-ARGS=$(getopt -n "install.sh" -o "hlb:x:" -l "help,list,bin:,exclude:" -- "$@")
+ARGS=$(getopt -n "install.sh" -o "hlp:x:" -l "help,list,prefix:,exclude:" -- "$@")
 set -- $ARGS
 while true; do
 	case $1 in
 		-h|--help)
 			printf "%s\n" "$DOC"
 			exit 0 ;;
-		-b|--bin)
+		-p|--prefix)
 			shift
-			INSTALLDIR="$(echo $1 | xargs)"
+			PREFIX="$(echo $1 | xargs)"
 			shift ;;
 		-l|--list)
 			shift
@@ -43,37 +50,38 @@ while true; do
 	esac
 done
 
-printf "This script will install the BreadyX utils (BXU) inside %s\nProceed? [y/n] " "$INSTALLDIR"
+printf "The BreadyX utils (BXU) will be installed in %s\n" "$PREFIX/*"
+printf "Proceed? [Y/n]"
 read -r i
-if [ "$(echo "$i" | tr "[:upper]" "[:lower]")" = "n" ]; then
-	printf "Aborted by user\n"
-	exit 0
-fi
-printf "\n"
-
-if { ! [ -d "$INSTALLDIR" ]; } && { ! [ -w "$INSTALLDIR" ]; } ; then
-	printf "Cannot access %s: directory doesn't exists or user hasn't access to it\n"\
-		"$INSTALLDIR"
-	exit 1
-fi
+if [ "$(echo "$i" | tr "[:upper]" "[:lower]")" = "n" ]; then exit 0; fi
 
 for proj in $PROJS; do
-	proj_dir="./src/$proj"
-	if [ "$EXCLUDE" != "${EXCLUDE#$proj}" ]; then # proj is in EXCLUDE
-		continue
-	fi
-	printf "%sInstalling '%s':%s\n" "$BOLD" "$proj" "$RESET"
-	if [ -x "$proj_dir/setup.sh" ]; then
-		( set +e
-		  cd "$proj_dir"
-		  out="$(sh -e setup.sh "$INSTALLDIR")" exit_code=$?
-		  printf "%s%s%s\n" "$GRAY" "$out" "$RESET" | fold | sed 's/^/\t/'
-		  if [ "$exit_code" != 0 ]; then
-			   printf "\t%sError in setup of %s%s\n" "$RED" "$proj" "$RESET"
-			   exit 1
-		  fi ) && printf "\t%sDone%s" "$GREEN" "$RESET"
-	else
-		printf "\t%sMissing setup for project '%s'%s\n" "$YELLOW" "$proj" "$RESET"
-	fi
 	printf "\n"
+
+	# esoteric way to check is $proj is in EXCLUDE. This is necessary as bash's
+	# arrays are quite a PITA
+	if [ "$EXCLUDE" != "${EXCLUDE#$proj}" ]; then continue; fi
+
+	proj_dir="./src/$proj"
+	printf "%sInstalling %s:%s\n" "$BOLD" "$proj" "$RESET"
+	if [ -f "$proj_dir/meson.build" ]; then
+		out="$(
+			cd "$proj_dir"
+			if [ -d "$BUILDDIR" ]; then
+				meson "$BUILDDIR" --reconfigure --prefix="$PREFIX" --buildtype=release
+			else
+				meson "$BUILDDIR" --prefix="$PREFIX" --buildtype=release
+		  	fi
+		  	ninja -C "$BUILDDIR" install
+		)"
+		exit_code=$?
+		printf "%s%s%s\n" "$GRAY" "$out" "$RESET" | sed "s/^/\t/"
+		if [ "$exit_code" == 0 ]; then
+			printf "\t%sDone%s\n" "$GREEN" "$RESET"
+		else
+			printf "\t%sError%s\n" "$RED" "$RESET"
+		fi
+	else
+		printf "\t%sMissing setup for %s%s\n" "$YELLOW" "$proj" "$RESET"
+	fi
 done
--- src/laptop-stuff/setup.sh
+++
@@ -1,7 +0,0 @@
-# Called by install.sh
-# First parameter is INSTALLDIR, cwd is ./src/laptop-stuff
-
-cp -u --preserve=mode ./toggle-wifi "$1/" >/dev/null 2>&1 || {
-	echo "Error: couldn't copy file"
-	exit 1
-}
